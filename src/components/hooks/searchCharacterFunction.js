b363026eaa8fb63d909e12e8a32a8688f6ec423e
NACXIIX <nicocordano@hotmail.com>
2025-07-11 22:08:34 -0300

    funcion arreglada para poder acceder a distintos atr del character encontrado

--- src/components/hooks/searchCharacterFunction.js
+++ src/components/hooks/searchCharacterFunction.js
@@ -2,19 +2,15 @@ export const searchCharacterFunction = (word, array) => {
     let newArray = []
     const new_word = word.trim().toLowerCase()
     let found = false
-    
+
     array.forEach(element => {
-        const new_element = element.trim().toLowerCase()
-        
-        if (new_word === new_element) {
+        const name = element.name?.trim().toLowerCase() || ""
+
+        if (name === new_word || name.includes(new_word)) {
             newArray.push(element)
             found = true
         }
-        if (new_element.includes(new_word)) {
-            newArray.push(element)
-            found = true
-        }
-    });
+    })
 
     return {
         newCharacters: newArray,
